44b91b35e6cf46a74d0c2d71f150d1ae0f53cc0c
JIN SVETLANA MARTIN-SCHATZ <jmartinschatz@gmail.com>
2014-03-11 00:37:09 -0400

    Change Carriable

--- Carriable.java
+++ Carriable.java
@@ -1,4 +1,10 @@
 /**
- * Carriable Interface
+ * Carriable Interface:
+ * for any carriable items in the place;
+ * store the location inside the Map;
+ * 
+ * Map:
+ *   1. Object name
+ *   2  int array
  * 
  */
