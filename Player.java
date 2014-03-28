79545f479e938ac8d724caae7d746b1cf30ef114
Terry Tuttle <ktuttle@gm.slc.edu>
2014-03-28 19:31:25 -0400

    Brainstorming
    Initial work to create Pack class and link it to the player class; a
    few other miscellaneous thoughts

--- Player.java
+++ Player.java
@@ -4,18 +4,14 @@ import java.awt.event.*; // MouseListener, MouseMotionListener, MouseEvent, KeyL
 import java.awt.*;       // Graphics, Graphics2D, Color
 import java.awt.geom.*;  // Ellipse2D
 
+/**
+ * TO DO:
+ * Work on loading image;
+ */
+
 public class Player extends JPanel {
-    
-    /**
-     * For now:
-     *    make a circle;
-     *    have the player(circle) move around);  
-     * 
-     */
-    
-    /**
-     * moves the character around
-     */
+   
+    // moves the character around
     private class ListenerForKeys implements KeyListener {
         public void keyPressed(KeyEvent event) {
             if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
@@ -39,17 +35,28 @@ public class Player extends JPanel {
     }
     
     // instance variables
-    int x, y;
-    int radius; // Temporary
+    private int x, y;
+    private int radius; // Temporary
+    public Place currentArea;
+//  private Pack pack; 
     
     public Player() {
         this.x = 200;
         this.y = 200;
         this.radius = 5;
+        this.currentArea = null;
         this.addKeyListener(new ListenerForKeys());
         this.setFocusable(true);
-//        this.setBackground(Color.TRANSLUCENT);
     }
+    
+    public String toString() {
+        return String.format("CURRENTLY IN: %s\nITEMS ON HAND: (Nothing yet)\n", this.currentArea.getName());
+    }
+    
+    public void currentlyIn (Place place) {
+        this.currentArea = place;
+    }
+    
     
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -57,9 +64,13 @@ public class Player extends JPanel {
         int x1 = this.x - this.radius;
         int y1 = this.y - this.radius;
         int diameter = this.radius * 2;
-        Ellipse2D.Double ball = new Ellipse2D.Double(x1, y1, diameter, diameter);
-        pen.fill(ball);
+        
+//        ImageIcon test = new ImageIcon("Test Tome left 1.png");
+//        test.paintIcon(this, g, 100, 100);
+        Ellipse2D.Double player = new Ellipse2D.Double(x1, y1, diameter, diameter);
+        pen.fill(player);
     }
+<<<<<<< HEAD
     
     public Pack openPack() {
       return this.Pack();
@@ -96,3 +107,6 @@ public class Player extends JPanel {
     }
     */
 }
+=======
+}
+>>>>>>> FETCH_HEAD
