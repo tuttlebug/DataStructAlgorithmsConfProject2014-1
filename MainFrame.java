1aa27d4021176d6ec27dc32be2e1e00b1feca899
JIN SVETLANA MARTIN-SCHATZ <jmartinschatz@gmail.com>
2014-04-02 16:17:43 -0400

    MainFrame planning

--- MainFrame.java
+++ MainFrame.java
@@ -1,12 +1,16 @@
 /*
- * The Main window for the game 
+ * The Main window for the game;
+ * 
+ * Check-List:
+ * [] set size;
+ * [] set name;
+ * [] set closing;
+ * 
+ * To Add/Implement:
+ * [] method that accepts new data and update the window
+ * 
  */
 
 public MainFrame extends JFrame {
-    /*
-     * Check-List:
-     * [] set size;
-     * [] set name;
-     * [] set closing;
-     */
+    
 }
