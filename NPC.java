6b0b8a03ed4ca6087962454156d8184c8c6eda06
JIN SVETLANA MARTIN-SCHATZ <jmartinschatz@gmail.com>
2014-05-09 13:19:19 -0400

    New Classes
    1) new Enemy and NPC classes. Both are subclasses of Interactable

---
+++ NPC.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.geom.*;
+import java.awt.Image;
+
+public class NPC extends Interactable {
+    
+    public NPC(String name, String imageFile) {
+        super(name, imageFile);
+        this.name = name;
+        this.imageFile = imageFile;
+        this.box = new Rectangle2D.Double();
+    }
+    
+}
